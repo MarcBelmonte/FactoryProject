c71badb79aa2866c057a925685dc1e4fac1fb223
MarcBelmonte <marc.belmonte.santacruz@gmail.com>
2018-10-10 21:37:48 +0200

    Intentando implementar log4j pero con problemas.

--- src/main/java/edu/upc/eetac/dsa/Factory.java
+++ src/main/java/edu/upc/eetac/dsa/Factory.java
@@ -1,16 +1,19 @@
 package edu.upc.eetac.dsa;
 
-import java.util.HashMap;
+import org.apache.log4j.Logger;
 
+import java.util.HashMap;
 public class Factory{
+
+    final static Logger log = Logger.getLogger(Factory.class.getName());
 
     private static final String PACKAGE = "edu.upc.eetac.dsa";
     private static Factory instance;
     private HashMap<String, Command> cache = new HashMap<String, Command>();
 
-
-    private Factory(){
-    }
+       private Factory(){
+           this.cache = new HashMap<String, Command>();
+        }
 
     public static Factory getInstance() {
         if(instance == null) instance = new Factory();
@@ -19,9 +22,10 @@ public class Factory{
     public  Command getCommand(String idCommand){
         Command c = cache.get(idCommand);
         if (c==null){
+            log.info("Cargador de clases");
             c = getCommand2(idCommand);
             cache.put(idCommand,c);
-        }else System.out.println("Hola");
+        }else log.info("Cache");
 
         return c;
     }
@@ -32,7 +36,15 @@ public class Factory{
             c = Class.forName(PACKAGE+"."+idCommand);
             command = (Command)c.newInstance();
 
-        } catch (Exception e){
+        } catch (InstantiationException e) {
+            // logger
+            log.error(e.getMessage());
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            log.error(e.getMessage());
             e.printStackTrace();
         }
         return command;
--- src/test/java/edu/upc/eetac/dsa/FactoryTest.java
+++ src/test/java/edu/upc/eetac/dsa/FactoryTest.java
@@ -1,8 +1,11 @@
 package edu.upc.eetac.dsa;
 
+import org.apache.log4j.Logger;
 import org.junit.*;
 
 public class FactoryTest {
+
+   final static Logger log = Logger.getLogger(Factory.class.getName());
    @Before
    public void setUp() {
 
@@ -10,6 +13,8 @@ public class FactoryTest {
 
    @Test
    public void testFactory(){
+
+
       Command c = Factory.getInstance().getCommand("C1"); // Classloader (C1)
       Assert.assertEquals("C1",c.execute());
 
